d4a73e666745b8f42f8baad87f70ca0b34dff5ac
Leonardo Sima <leosima.lc@gmail.com>
2021-12-28 16:18:52 -0300

    Console
    Algumas anotações sobre o console, para que serve , sua importância e como usá-lo (além do console.log)

---
+++ tipos-variaveis-funcoes/ex009-console.js
@@ -0,0 +1,20 @@
+// Console.api
+
+//log = registro
+// O console é usado para exibir dados do nosso código, sem que o usuário veja.
+
+const minhaVar = true
+
+console.log(10)
+console.log("String")
+console.log(minhaVar)
+
+// tratamento de erro
+
+console.error('\nDeu erro, mané!')
+
+// console.log() => Registros de forma geral
+// console.error() => Para exibir mensagens de erro
+// console.table() => Para visualizar dados tabulares de forma mais organizada
+// console.time() e console.timeEnd() => Para temporizar o período de execução de um código
+// console.trace() => Para exibir a stacktrace de todos os pontos por onde o código executado passou
