1e8227107eb75ee83344b45dbf3d34a8addc5570
Leonardo Sima <leosima.lc@gmail.com>
2021-12-28 15:22:33 -0300

    Entendendo JS e NodeJS
    Algumas anotações sobre noções básicas do que é o JS e NodeJS.

---
+++ tipos-variaveis-funcoes/ex007-entendendo-js.js
@@ -0,0 +1,11 @@
+// Tipagem Dinâmica
+
+let minhaVar = 567; // Number
+minhaVar = "string"; // String
+minhaVar = true // Bool
+
+// Não é necessário definir qual é o tipo da variável, o JavaScript determina isso sozinho, por isso é de tipagem dinâmica. Por isso, tome cuidado com suas variáveis.
+// Java != JavaScript
+// ES6/ES2015 => É a sexta versão do JavaScript, lançada em 2015, sendo a mais famosa, trazendo features importantes que alavancaram o uso de JavaScript
+// JavaScript é uma linguagem interpretada, isto é, um programa vai ler a linha de código e excutá-la. Diferente da linguagem compilada, que precisa que o código passe por um compilador que traduz o código para uma linguagem de máquina, para que enfim o programa leia o código já "traduzido"
+// NodeJS é um interpretador de JavaScript com algumas funções interessantes, geralmente usado para códigos JavaScript fora do navegador
