483f35cf0c971ab1ec0d197004baa04fa953e27d
Leonardo Sima <leosima.lc@gmail.com>
2021-12-30 16:18:09 -0300

    Template String

---
+++ tipos-variaveis-funcoes/ex012-template-string.js
@@ -0,0 +1,13 @@
+// Template String
+
+const nome = "Leo"
+const idade = 2021 - 2002
+const cidadeNascimento = "Curitiba"
+
+// const apresentacao = "Meu nome é " + nome + ", tenho " + idade + " anos de idade e nasci em " + cidadeNascimento
+
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos de idade e nasci na cidade de ${cidadeNascimento}`
+
+console.log(apresentacao)
+
+
